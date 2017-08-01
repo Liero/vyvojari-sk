e53e26ba1279cbb1036528286ee6ea3488c3ecd7
Daniel Turan (g) <daniel.turan@avanade.com>
2017-08-01 15:32:08 +0200

    Added markdown rendering

---
+++ src/DevPortal/TagHelpers/MarkdownTagHelper.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevPortal.Web.TagHelpers
+{
+    [HtmlTargetElement("p", Attributes = "markdown")]
+    [HtmlTargetElement("markdown")]
+    [OutputElementHint("p")]
+    public class MarkdownTagHelper : TagHelper
+    {
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            if (output.TagName == "markdown")
+            {
+                output.TagName = null;
+            }
+            output.Attributes.RemoveAll("markdown");
+
+            string markdownContent = (await output.GetChildContentAsync()).GetContent();
+            string html = CommonMark.CommonMarkConverter.Convert(markdownContent);
+            output.Content.SetHtmlContent(html ?? "");
+        }
+    }
+}
