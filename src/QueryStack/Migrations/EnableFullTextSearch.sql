9abb5ee01f736697b2ed0ec8b3ab93c92cf33f61
liero <turand@centrum.sk>
2020-01-04 15:28:28 +0100

    Fixed Fulltext search

---
+++ src/QueryStack/Migrations/EnableFullTextSearch.sql
@@ -0,0 +1,5 @@
+﻿-- adding this to EF Migrations caused publis to fail, so it has to be executed manually or as part of CI/CD pipeline
+CREATE FULLTEXT CATALOG ft AS DEFAULT
+GO
+CREATE FULLTEXT INDEX ON dbo.ContentBase(Content, Title, Description) KEY INDEX [PK_ContentBase] WITH STOPLIST = SYSTEM
+GO
