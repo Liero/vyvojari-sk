e3ab22a92dbe83c7a7fbce5f69dd3a7837f31175
liero <turand@centrum.sk>
2020-01-05 10:02:18 +0100

    Added SendGrid and SignIn.RequireConfirmedEmail

---
+++ src/DevPortal/AppCode/Config/SendGrid.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevPortal.Web.AppCode.Config
+{
+    public class SendGrid
+    {
+        public string User { get; set; }
+        public string Key { get; set; }
+    }
+}
--- src/DevPortal/Services/IEmailSender.cs
+++ src/DevPortal/Services/IEmailSender.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.Extensions.Options;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text.Encodings.Web;
@@ -8,22 +11,52 @@ namespace DevPortal.Web.Services
 {
     public interface IEmailSender
     {
-        Task SendEmailAsync(string email, string subject, string message);
+        Task SendEmailAsync(IEnumerable<string> email, string subject, string message);
     }
 
     public class EmailSender : IEmailSender
     {
-        public Task SendEmailAsync(string email, string subject, string message)
+        private AppCode.Config.SendGrid _config;
+        public EmailSender(IOptions<AppCode.Config.SendGrid> sendGridOptions)
         {
-            return Task.FromResult(0);
+            _config = sendGridOptions.Value;
+        }
+
+        public Task SendEmailAsync(IEnumerable<string> emails, string subject, string message)
+        {
+
+            return Execute(_config.Key, subject, message, emails);
+        }
+
+        public Task Execute(string apiKey, string subject, string message, IEnumerable<string> emails)
+        {
+            var client = new SendGridClient(apiKey);
+            var msg = new SendGridMessage()
+            {
+                From = new EmailAddress("notifications@vyvojari.sk", "Vyvojari.sk"),
+                Subject = subject,
+                PlainTextContent = message,
+                HtmlContent = message
+            };
+
+            foreach (var email in emails)
+            {
+                msg.AddTo(new EmailAddress(email));
+            }
+
+            Task response = client.SendEmailAsync(msg);
+            return response;
         }
     }
+
+
+
 
     public static class EmailSenderExtensions
     {
         public static Task SendEmailConfirmationAsync(this IEmailSender emailSender, string email, string link)
         {
-            return emailSender.SendEmailAsync(email, "Confirm your email",
+            return emailSender.SendEmailAsync(new[] { email }, "Confirm your email",
                 $"Please confirm your account by clicking this link: <a href='{HtmlEncoder.Default.Encode(link)}'>link</a>");
         }
     }
